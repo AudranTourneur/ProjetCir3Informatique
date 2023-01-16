2bd952606cb8449534742b1d8373cdb243fe0362
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-01-16 22:09:51 +0100

    TRPC Setup

--- back/src/server.ts
+++ back/src/server.ts
@@ -73,6 +73,10 @@ app.post('/resetPassword', function (req, res) {
     // account.resetPassword(req.body.token, req.body.password, req.body.language, con, res);
 });
 
+app.get('/', (req, res) => {
+    res.send('Up and running!')
+})
+
 
 let counter = 0
 app.get('/test', (req, res) => {
@@ -80,7 +84,76 @@ app.get('/test', (req, res) => {
     res.send(JSON.stringify({ a: counter++}));
 })
 
-if (app.listen(process.env.PORT || 8080)) {
+
+
+import { z } from 'zod';
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+
+// created for each request
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
+
+const router = t.router;
+const publicProcedure = t.procedure;
+
+interface User {
+  id: string;
+  name: string;
+}
+
+const userList: User[] = [
+  {
+    id: '1',
+    name: 'TEST USER',
+  },
+];
+
+const appRouter = router({
+  userById: publicProcedure
+    .input((val: unknown) => {
+      if (typeof val === 'string') return val;
+      throw new Error(`Invalid input: ${typeof val}`);
+    })
+    .query((req) => {
+      const input = req.input;
+      const user = userList.find((it) => it.id === input);
+
+      return user;
+    }),
+  userCreate: publicProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation((req) => {
+      const id = `${Math.random()}`;
+
+      const user: User = {
+        id,
+        name: req.input.name,
+      };
+
+      userList.push(user);
+
+      return user;
+    }),
+});
+
+export type AppRouter = typeof appRouter;
+
+app.use('/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  }),
+)
+
+const port = process.env.PORT || 8080
+
+if (app.listen(port)) {
     console.log('=========== SERVER STARTED FOR HTTP RQ ===========');
-    console.log('    =============   PORT: 8080   =============');
+    console.log(`    =============   PORT: ${port}   =============`);
 }
