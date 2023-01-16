7b311ec265131ca9e7e875515aa2fe14dbfbe008
AudranTourneurIsen <audran.tourneur@student.junia.com>
2023-01-16 14:28:01 +0100

    example

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,1 +1,16 @@
-Bonjour
+<script>
+    let inputText = ""
+</script>
+
+<button class="btn btn-primary">test</button>
+
+<div class="bg-green-400 w-32 h-32 rounded-xl"></div>
+
+<p class="text-3xl text-red-500 underline">test</p>
+
+<input type="checkbox" class="toggle">
+
+test
+<input type="text" class="input input-primary" bind:value={inputText}>
+
+<h1>valeur tapé : {inputText}</h1>
