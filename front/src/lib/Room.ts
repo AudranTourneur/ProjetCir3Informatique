39ce30081678a9356f2a760f8f7a8bccba375e89
Melian-e\Administrateur <iemelian.rambeau@student.junia.com>
2023-01-17 12:10:02 +0100

    push classes non fonctionnelle

--- front/src/lib/Room.ts
+++ front/src/lib/Room.ts
@@ -1,13 +1,28 @@
+import * as d3 from "d3";
+
 export class Room {
-    points: Array<Array<Number>>;
-    capacite: Number;
-    projecteur: Boolean;
-    
-    constructor(points) {
-        this.points = points;
+    public polygon : undefined | d3.Selection<SVGPolygonElement, unknown, HTMLElement, any>;
+
+    constructor(private points : Number[][], public name : String, public capacity: Number, public projecteur: Boolean) {
+    }
+
+    draw() {
+        let svg = d3.select("svg");
+
+        this.polygon = svg.append("polygon")
+        .attr('points', this.points.toString())
+        .attr('stroke', '#f00')
+        .attr('fill', 'red')
+        .style("fill-opacity", .2)
+        .on("click", () => {
+            d3.selectAll("svg > *").remove();
+            this.polygon!.attr('stroke', '#ff0');
+        });
     }
     
     printRoom() {
         console.log(this.points);
     }
+
+
 }
