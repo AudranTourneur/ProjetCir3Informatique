8f1b2fdc1656108e5218ce4b23c766b89220f780
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-01-18 16:34:40 +0100

    types

--- back/src/types.ts
+++ back/src/types.ts
@@ -16,9 +16,33 @@ export type Room ={
 }
 
 export type Plan = {
-    id: number,
+    id: string,
+    imageId: string,
     name: string,
     description: string,
     rooms: Array<Room>,
     isPublic: boolean,
 }
+
+import { z } from "zod";
+
+// creating a schema for strings
+export const planSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    description: z.string(),
+    imageId: z.string(),
+    rooms: z.array(z.object({
+        name: z.string(),
+        points: z.array(z.object({  
+            x: z.number(),
+            y: z.number(),
+        })),
+        capacity: z.number(),
+        hasProjector: z.boolean(),
+        hasWhiteboard: z.boolean(),
+        hasBlackboard: z.boolean(),
+        description: z.string(),
+    })),
+    isPublic: z.boolean(),
+});
