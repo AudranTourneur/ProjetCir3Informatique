f58cdf87b9e7e909df5bd9fbafd022c075046334
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-01-18 20:22:11 +0100

    plan creation

--- front/src/routes/admin/editor/ModalPlanCreate.svelte
+++ front/src/routes/admin/editor/ModalPlanCreate.svelte
@@ -12,7 +12,7 @@
     
     let lastImageId = null;
 
-   export let images;
+   export let images: any[];
 
    let lastUploadedImage: any | null = null;
 
@@ -35,40 +35,80 @@
         return res.id
     }
 
-    async function create() {
-        if (!lastUploadedImage) return;
+    let isLoading = false;
+
+    const method = 'POST'
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+
+    async function createWithUploadedImage() {
         const imageId = await upload();
         if (!imageId) return;
 
-        fetch(`${PUBLIC_API_HOST}/createPlan`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: 'Plan 1',
-                imageId,
-                rooms: [],
-                description: '',
-                isPublic: false,
-            })
-        }
+        if (!lastUploadedImage) return;
 
+        console.log('lastUpload', lastUploadedImage)
+
+        await fetch(`${PUBLIC_API_HOST}/createPlan`, {
+            method, headers,
+            body: JSON.stringify({
+                name: toCreate.name,
+                imageId,
+            })
+        })
+
+        toCreate = null
+    }
+
+    async function createWithSelectedImage() {
+        if (!selectedImage) return;
+        console.log(selectedImage, toCreate)
+        await fetch(`${PUBLIC_API_HOST}/createPlan`, {
+            method, headers,
+            body: JSON.stringify({
+                name: toCreate.name,
+                imageId: selectedImage,
+            })
+        })
 
         toCreate= null
     }
 
+    async function create() {
+        console.log('start upload', tabActive)
+
+        isLoading = true
+
+        if (tabActive === 'select')
+            await createWithSelectedImage()
+        else if (tabActive === 'upload')
+            await createWithUploadedImage()
+        
+        isLoading = false
+    }
+
     let isButtonDisabled = true;
 
+    let selectedImage: any | null = null;
+
     $: {
-        if (!lastUploadedImage) 
-            isButtonDisabled = true
-        else
+        if (lastUploadedImage || selectedImage)
             isButtonDisabled = false
+        else
+            isButtonDisabled = true
+        console.log('bjr ici', isButtonDisabled)
     }
+
+    function selectImage(image: any) {
+        selectedImage = image;
+    }
+
+    let tabActive: 'upload' | 'select' = 'upload'
   
 </script>
 
+{#if toCreate}
 <Modal bind:isActive>
     <div slot="title" class="text-2xl">Création de plan</div>
     <div slot="content" class="w-full">
@@ -76,23 +116,28 @@
         <label for="input-nom" class="label">
           <span class="label-text">Nom du plan</span>
         </label>
-        <input id="input-nom" type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        <input bind:value={toCreate.name} id="input-nom" type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
     </div>
 
-        <div class="flex flex-col w-full lg:flex-row">
+        <div class="tabs">
+          <button class="tab tab-bordered" class:tab-active={tabActive === 'upload'} on:click={() => tabActive = 'upload'}>Téleverser une image</button> 
+          <button class="tab tab-bordered" class:tab-active={tabActive === 'select'} on:click={() => tabActive = 'select'}>Sélectionner une image existante</button>
+        </div>
+
+        <div class:hidden={tabActive !== 'upload'}>
             <ImageUpload bind:lastUploadedImage />
-            <div class="divider lg:divider-horizontal">OU</div> 
-            <div class="flex flex-col overflow-y-scroll h-[40vh]">
+        </div>
+            <div class="flex-1 overflow-y-scroll h-[40vh]" class:hidden={tabActive !== 'select'} >
                 {#each images as image}
-                    <div class="flex flex-grow h-32 rounded-box place-items-center">
-                        <img src="{PUBLIC_API_HOST}/images/{image}" alt="" width="300" height="300">
-                    </div>
+                    <button class="w-40 h-40  {selectedImage === image ? 'border-4 border-red-500' : 'hover:border hover:border-red-500'} border-2 hover:cursor-pointer" on:click={() => selectImage(image)}>
+                        <img src="{PUBLIC_API_HOST}/images/{image}.png?miniature=true" alt="" width="300" height="300">
+                    </button>
                 {/each}
             </div>
-        </div>
     </div>
     <div slot="buttons">
         <button class="btn" on:click={()=> toCreate = null}>Annuler</button>
-        <button class="btn btn-success" on:click={create} disabled={isButtonDisabled}>Créer</button>
+        <button class="btn btn-success" on:click={create} disabled={isButtonDisabled} class:loading={isLoading}>Créer</button>
     </div>
 </Modal>
+{/if}
