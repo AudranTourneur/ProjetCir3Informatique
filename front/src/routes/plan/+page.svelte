4c6d2778bb5d8d1777be37e942eb61068c9ee49d
Melian-e\Administrateur <iemelian.rambeau@student.junia.com>
2023-01-19 15:59:43 +0100

    psuh aveec scss

--- front/src/routes/plan/+page.svelte
+++ front/src/routes/plan/+page.svelte
@@ -6,7 +6,10 @@
 	import { writable, type Writable } from 'svelte/store';
 	import { slide } from 'svelte/transition';
 	import TimePlan from '../plan/TimePlan.svelte';
-    import { Datepicker } from 'svelte-calendar'
+    import LoginModal from '$lib/LoginModal.svelte';
+	import { Datepicker } from 'svelte-calendar';
+	
+	let isLoginModalOpen = false;
 
     let el : HTMLDivElement;
 
@@ -97,8 +100,79 @@
 </script>
 
 
-
 <div class="absolute overflow-hidden" bind:this={el}></div>
+
+<!-- Floor input -->
+<div class="flex flex-col absolute p-[15px] m-0 gap-[15px] z-10">
+    <div class="btn__container">
+        <div class="dropdown">
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <!-- svelte-ignore a11y-label-has-associated-control -->
+            <label tabindex="0" class="btn m-1">Click</label>
+            <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li><button>Item 1</button></li>
+                <li><button>Item 2</button></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<!-- Filter DATEPICKER -->
+<div id="filterContainer" class="absolute left-[30vw] w-[40vw] overflow-hidden rounded-t-lg p-[25px] z-0">
+    <div class="flex justify-center">
+        <div class="filter__container">
+
+            <!-- DATE PICKER -->
+            <div class="flex items-center justify-center p-[15px]">
+                <div class="datepicker flex flex-row gap-[10px] items-center" data-mdb-toggle-button="false">
+                    <Datepicker format="DD/MM/YYYY"></Datepicker>
+                    <i class='bx bx-calendar'></i>
+                    <input id="dateInput" type="text"
+                            class="form-control w-32 block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            placeholder="Select a date"
+                            data-mdb-toggle="datepicker"
+                    />
+                </div>
+            </div>
+
+            <div class="h-full border-r-[1px] border-[#959896]"></div>
+            <div class="p-[15px]">
+                <i id="searchBtn" class='bx bx-search-alt'></i>
+            </div>
+
+        </div>
+    </div>
+
+    <!--<div id="searchResult" class="search__container">
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+    </div>-->
+
+</div>
+
+<LoginModal bind:isActive={isLoginModalOpen} />
+
+<!-- Aside btn (Compte / dark mode ...) -->
+<div class="flex flex-col absolute right-0 p-[25px] m-0 gap-[15px] z-10">
+    <div class="btn__container">
+        <div class="p-[10px]">
+            <i class='bx bx-user' on:click={() => isLoginModalOpen = true}></i>
+        </div>
+        <div class="border-t-[1px] border-[#959896]"></div>
+        <div class="p-[10px]">
+            <i class='bx bx-moon'></i>
+        </div>
+    </div>
+
+    <div class="btn__container">
+        <div class="p-[10px]">
+            <!--<i class='bx bxs-user' on:click={() => isLoginModalOpen = true}></i>-->
+            <i class='bx bx-crown' ></i>
+        </div>
+    </div>
+</div>
 
 <!-- Span bottom edge -->
 <div class="absolute left-0 bottom-0 w-full">
@@ -147,11 +221,95 @@
             </div>
         </div>
     {/if}
-  </div>
+</div>
 </div>
 
-<style>
+<style lang="scss">
     :global(html, body) {
         overflow: hidden;
     }
+    :root {
+  /* Responsive Elements */
+  --fs-error: 10rem;
+  --fs-3xl: 5rem;
+  --fs-xxl: 3rem;
+  --fs-xl: 2.5rem;
+  --fs-l: 2rem;
+  --fs-m: 1.5rem;
+  --fs-sm: 1.25rem;
+  --fs-s: 1rem;
+  --fs-ss: 0.85rem;
+}
+
+main {
+  overflow: hidden;
+  background-color: white;
+}
+
+.map__responsive {
+  overflow: hidden;
+
+  iframe {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+  }
+}
+
+.btn__container {
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  background-color: #313536;
+  box-shadow: 0 0 3px #313536;
+
+  i {
+    color: #959896;
+    font-size: var(--fs-l);
+    &:hover {
+      cursor: pointer;
+      opacity: .75
+    }
+  }
+}
+
+.filter__container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: #313536;
+  border-radius: 30px;
+  color: white;
+
+  i {
+    font-size: var(--fs-l);
+  }
+}
+
+.search__container {
+  display: flex;
+  flex-direction: column;
+  padding: 25px;
+}
+
+#slideBtn {
+  cursor: pointer;
+  font-size: var(--fs-l);
+  color: white;
+  &:hover {
+    opacity: .75;
+  }
+}
+
+#searchBtn {
+  cursor: pointer;
+  font-size: var(--fs-l);
+  color: white;
+  &:hover {
+    opacity: .75;
+  }
+}
+
 </style>
