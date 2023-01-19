c0f83a395cd518734b99798678af3e55a598721b
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-01-19 09:36:08 +0100

    changed sign-in/register error style

--- front/src/lib/LoginModal.svelte
+++ front/src/lib/LoginModal.svelte
@@ -57,6 +57,9 @@
         isRegisterModal = !isRegisterModal;
     }
 
+	let loginErrorOutput = ''
+	let registerErrorOutput = ''
+
 	async function sendData(){
 		let url = undefined;
 		// login
@@ -78,16 +81,16 @@
 						switch(data.status){
 							case 0:
 								console.info("user does not exist!");
-								document.getElementById("loginErrorOutput").textContent = "user does not exist!"
+								loginErrorOutput = "User does not exist!"
 								break;
 							case 1:
-								console.info("user connected!");
+								console.info("user connected!", data);
 								//localStorage.setItem("token", data.token);
-								location.href = "/"
+								//location.href = "/"
 								break;
 							case 2:
 								console.info("invalid password!");
-								document.getElementById("loginErrorOutput").textContent = "invalid password!"
+								loginErrorOutput = "Invalid password!"
 								break;
 							default:
 								break;
@@ -129,11 +132,11 @@
 								break;
 							case 1:
 								console.info("user already registered with that mail!");
-								document.getElementById("registerErrorOutput").textContent = "user is already registered!"
+								registerErrorOutput = "User is already registered!"
 								break;
 							case 2:
 								console.info("not data received in the header!")
-								document.getElementById("registerErrorOutput").textContent = "not data sent in the header"
+								registerErrorOutput = "Not data sent in the header"
 								break;
 							default:
 								break;
@@ -159,7 +162,7 @@
 		>
 
 			<div class="flex justify-center items-center h-full">
-				<div class="flex flex-col items-center w-[380px] h-[400px] bg-base-200 rounded-xl px-4 gap-3">
+				<div class="flex flex-col items-center w-[380px] h-[460px] bg-base-200 rounded-xl px-4 gap-3">
 					<div class="flex justify-between items-center w-full my-4">
 						<div />
 						<h1 class="text-3xl font-bold">Sign in</h1>
@@ -168,9 +171,14 @@
 						</button>
 					</div>
 
-					<div class="bg-red-500 w-[340px]">
-						<pre id="loginErrorOutput"></pre>
+					{#if loginErrorOutput}
+					<div class="alert alert-error shadow-lg">
+					  <div>
+					    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+					    <span>{loginErrorOutput}</span>
+					  </div>
 					</div>
+					{/if}
 
 					<div class="form-control w-full max-w-xs">
 						<label class="label">
@@ -190,7 +198,7 @@
 						<button on:click={() => sendData()} class="btn btn-md btn-success" class:loading={isLoading} on:click={() => isLoading = true}>Sign in</button>
 					</div>
 
-					<pre on:click={()=> changeModal()}>Créer un compte</pre>
+					<button class="btn btn-link" on:click={()=> changeModal()}>Créer un compte</button>
 
 				</div>
 			</div>
@@ -207,7 +215,7 @@
 		>
 
 			<div class="flex justify-center items-center h-full">
-				<div class="flex flex-col items-center w-[380px] h-[400px] bg-base-200 rounded-xl px-4 gap-3">
+				<div class="flex flex-col items-center w-[380px] h-[460px] bg-base-200 rounded-xl px-4 gap-3">
 					<div class="flex justify-between items-center w-full my-4">
 						<div />
 						<h1 class="text-3xl font-bold">Create an account</h1>
@@ -216,9 +224,15 @@
 						</button>
 					</div>
 
-					<div class="bg-red-500 w-[340px]">
-						<pre id="registerErrorOutput"></pre>
-					</div>
+					{#if registerErrorOutput}
+						
+						<div class="alert alert-error shadow-lg">
+						  <div>
+						    <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+						    <span>{registerErrorOutput}</span>
+						  </div>
+						</div>
+					{/if}
 
 					<div class="form-control w-full max-w-xs">
 						<label class="label">
@@ -238,7 +252,7 @@
 						<button on:click={()=> sendData()} class="btn btn-md btn-success" class:loading={isLoading} on:click={() => isLoading = true}>Register</button>
 					</div>
 
-					<pre on:click={()=> changeModal()}>Se connecter</pre>
+					<button class="btn btn-link" on:click={()=> changeModal()}>Créer un compte</button>
 
 				</div>
 			</div>
