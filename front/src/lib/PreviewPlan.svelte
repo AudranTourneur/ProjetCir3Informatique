b92aed2fa735271fe4379f7f18c43d108fb56953
AudranTourneur <108300467+AudranTourneur@users.noreply.github.com>
2023-01-20 13:30:58 +0100

    edit

--- front/src/lib/PreviewPlan.svelte
+++ front/src/lib/PreviewPlan.svelte
@@ -46,7 +46,7 @@
 								>Supprimer</button
 							>
 
-				<a href="/plan/{plan._id}">
+				<a href="/admin/{plan._id}">
 					<button class="btn btn-outline btn-info">Modifier</button>
 				</a>
 				{/if}
